777e2ba4170570301281e8f0044ceaeaa6995619
Kaan Er <erkaan1@outlook.com>
2021-01-28 18:01:46 +0300

    Add react router setup and add navbar,landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import { Fragment } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
