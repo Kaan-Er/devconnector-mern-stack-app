b3bb366d195b54a6547d943e2a3b56bc7e6cb245
Kaan Er <erkaan1@outlook.com>
2021-01-31 16:21:53 +0300

    Create profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import { setAlert } from "./alert";
-import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import axios from 'axios';
+import { setAlert } from './alert';
+import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current users profile
 export const getCurrentProfile = () => async (dispatch) => {
   try {
-    const res = await axios.get("/api/profile/me");
+    const res = await axios.get('/api/profile/me');
 
     dispatch({
       type: GET_PROFILE,
@@ -14,22 +14,47 @@ export const getCurrentProfile = () => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status },
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
     });
   }
 };
 
 // Create or update profile
-export const createProfile = (FormData, history, edit = false) => async (
+export const createProfile = (formData, history, edit = false) => async (
   dispatch
 ) => {
   try {
     const config = {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     };
 
-    const res = await axios.post("/api/profile", FormData);
-  } catch (err) {}
+    const res = await axios.post('/api/profile', formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
 };
